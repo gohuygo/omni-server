2d6fdabfd31fdbcc464a3d1118d95b89f19b4cb2
gohuygo <huy@gohuygo.com>
2018-12-12 13:36:32 -0800

    Minor nit formatting

--- passport.js
+++ passport.js
@@ -1,5 +1,5 @@
 // https://medium.com/front-end-hacking/learn-using-jwt-with-passport-authentication-9761539c4314
-const passport = require('passport’);
+const passport = require('passport');
 
 const LocalStrategy = require('passport-local').Strategy;
 
@@ -7,12 +7,11 @@ passport.use(
   new LocalStrategy({usernameField: 'email', passwordField: 'password'},
   function (email, password, cb) {
     return UserModel.findOne({email, password})
-       .then(user => {
-           if (!user) {
-               return cb(null, false, {message: 'Incorrect email or password.'});
-           }
-           return cb(null, user, {message: 'Logged In Successfully'});
-      })
-      .catch(err => cb(err));
+      .then(user => {
+        if (!user) {
+          return cb(null, false, {message: 'Incorrect email or password.'});
+        }
+        return cb(null, user, {message: 'Logged In Successfully'});
+      }).catch(err => cb(err));
   }
 ));
