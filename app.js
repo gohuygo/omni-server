28d58ac343ad9842d714c585354b8263c72b44ca
gohuygo <huy@gohuygo.com>
2018-12-12 13:16:18 -0800

    Successfully signed up user

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express')
-const app     = express()
+const models  = require('./db/models/index');
+const bcrypt  = require('bcrypt');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 8080
 
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/users', async (req, res) => {
+  let encryptedPassword = await bcrypt.hash(req.body.password, 10);
+  console.log(req.body.password, encryptedPassword);
+  models.user.create({
+    email: req.body.email,
+    name: req.body.name,
+    password: encryptedPassword,
+  })
 })
 
 app.listen(port, () => {
