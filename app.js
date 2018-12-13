414385c52f55a3cd893a7f496a62a84b2523df8e
Huy <huy@gohuygo.com>
2018-12-12 19:01:51 -0800

    Feature/passport js (#1)
    * Setting up passport.js
    
    * Minor nit formatting
    
    * Working passport registration and authentication
    
    * Refactor

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const models  = require('./db/models/index');
 const bcrypt  = require('bcrypt');
 const bodyParser = require('body-parser');
 
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = models.user;
+const router = express.Router();
+
+// TODO: Change and move to env variable
+const secret = "hello1234"
+
+require('./passport.js');
+
 const port = process.env.PORT || 8080
 
 const app = express()
@@ -10,19 +20,54 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-app.post('/users', async (req, res) => {
-  let encryptedPassword = await bcrypt.hash(req.body.password, 10);
-  console.log(req.body.password, encryptedPassword);
-  models.user.create({
-    email: req.body.email,
-    name: req.body.name,
-    password: encryptedPassword,
-  })
-})
+app.post('/login', (req, res) => {
+  passport.authenticate('local', { session: false }, (error, user) => {
+    if (error || !user) {
+      res.status(400).json({ error });
+    }
+
+    // Construct JWT payload
+    const payload = {
+      email: user.email,
+      expires: Date.now() + parseInt(process.env.JWT_EXPIRATION_MS),
+    };
+
+    // Assign payload to req.user
+    req.login(payload, {session: false}, (error) => {
+      if (error) {
+        res.status(400).send({ error });
+      }
+
+      // Generate a signed JWT
+      const token = jwt.sign(JSON.stringify(payload), secret);
+
+      // Assign JWT to cookie
+      res.cookie('jwt', token, { httpOnly: true, secure: true });
+      res.status(200).send({ email: user.email });
+    });
+  })(req, res);
+});
+
+app.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const userDocument = User.create({ email: email, password: passwordHash });
+
+    res.status(200).send({ email });
+
+  } catch (error) {
+    res.status(400).send({
+      error: 'req body should take the form { email, password }',
+    });
+  }
+});
+
 
 app.listen(port, () => {
  console.log(`Server listening on port: ${port}`)
---
+++ passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const JWTStrategy = require('passport-jwt').Strategy
+const bcrypt = require('bcrypt')
+
+// TODO: Change and move to env variable
+const secret = "hello1234"
+
+const User = require('./db/models/index').user;
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  session: false,
+}, async (email, password, done) => {
+  try {
+    const user = await User.findOne({ where: {email: email} })
+    const passwordsMatch = await bcrypt.compare(password, user.password)
+
+    if (passwordsMatch) {
+      return done(null, user)
+    } else {
+      return done('Incorrect Email / Password')
+    }
+  } catch (error) {
+    done(error)
+  }
+}))
+
+passport.use(new JWTStrategy(
+  { jwtFromRequest: req => req.cookies.jwt, secretOrKey: secret},
+  (jwtPayload, done) => {
+    if (jwtPayload.expires > Date.now()) {
+      return done('jwt expired')
+    }
+    return done(null, jwtPayload)
+  }
+))
