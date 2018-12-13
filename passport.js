93ab2418966223dc45a2e63d2278a59f36f138f8
gohuygo <huy@gohuygo.com>
2018-12-12 17:23:52 -0800

    Working passport registration and authentication

--- passport.js
+++ passport.js
@@ -1,17 +1,42 @@
-// https://medium.com/front-end-hacking/learn-using-jwt-with-passport-authentication-9761539c4314
-const passport = require('passport');
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const JWTStrategy = require('passport-jwt').Strategy
+const bcrypt = require('bcrypt')
 
-const LocalStrategy = require('passport-local').Strategy;
+// TODO: Change and move to env variable
+const secret = "hello1234"
 
-passport.use(
-  new LocalStrategy({usernameField: 'email', passwordField: 'password'},
-  function (email, password, cb) {
-    return UserModel.findOne({email, password})
-      .then(user => {
-        if (!user) {
-          return cb(null, false, {message: 'Incorrect email or password.'});
-        }
-        return cb(null, user, {message: 'Logged In Successfully'});
-      }).catch(err => cb(err));
+const User = require('./db/models/index').user;
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  session: false,
+}, async (email, password, done) => {
+  try {
+    const user = await User.findOne({ where: {email: email} })
+    const passwordsMatch = await bcrypt.compare(password, user.password)
+
+    if (passwordsMatch) {
+      return done(null, user)
+    } else {
+      return done('Incorrect Email / Password')
+    }
+  } catch (error) {
+    done(error)
   }
-));
+}))
+
+passport.use(new JWTStrategy({
+    jwtFromRequest: req => req.cookies.jwt,
+    secretOrKey: secret,
+  },
+  (jwtPayload, done) => {
+
+    if (jwtPayload.expires > Date.now()) {
+      return done('jwt expired')
+    }
+
+    return done(null, jwtPayload)
+  }
+))
