a45b08c0b802d3e5c2dc8f05eb73f85c5f165ab4
gohuygo <huy@gohuygo.com>
2018-12-14 15:17:14 -0800

    send cookies in payload

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const models  = require('./db/models/index');
 const bcrypt  = require('bcrypt');
 const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser')
 
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
@@ -17,6 +18,8 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// app.use(cookieParser());
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -24,15 +27,16 @@ app.use(function(req, res, next) {
 });
 
 app.post('/login', (req, res) => {
+  console.log(req)
   passport.authenticate('local', { session: false }, (error, user) => {
     if (error || !user) {
       res.status(400).json({ error });
     }
 
-    // Construct JWT payload
+    // Construct JWT payload with 10 minute expiration
     const payload = {
       email: user.email,
-      expires: Date.now() + parseInt(process.env.JWT_EXPIRATION_MS),
+      expires: Date.now() + parseInt(60000*10),
     };
 
     // Assign payload to req.user
@@ -40,23 +44,22 @@ app.post('/login', (req, res) => {
       if (error) {
         res.status(400).send({ error });
       }
-
       // Generate a signed JWT
       const token = jwt.sign(JSON.stringify(payload), process.env.JWT_SECRET);
 
       // Assign JWT to cookie
-      res.cookie('jwt', token, { httpOnly: true, secure: true });
-      res.status(200).send({ email: user.email });
+      res.cookie('jwt', token, { httpOnly: true, secure: false });
+      res.status(200).send({ email: user.email, jwt: token });
     });
   })(req, res);
 });
 
 app.post('/register', async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, name } = req.body;
 
   try {
     const passwordHash = await bcrypt.hash(password, 10);
-    const userDocument = User.create({ email: email, password: passwordHash });
+    const userDocument = User.create({ email: email, password: passwordHash, name: name });
 
     res.status(200).send({ email });
 
